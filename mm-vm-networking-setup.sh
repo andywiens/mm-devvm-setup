3af8a803b42e7f57ada545cf8d6dcae4bb33bbeb
Andy Wiens <andy@mindsandmachines.com>
2012-04-02 09:22:20 -0500

    fixed issue w/ path to tmp file

--- mm-vm-networking-setup.sh
+++ mm-vm-networking-setup.sh
@@ -16,5 +16,5 @@ sudo mv ~/tmp-network /etc/sysconfig/network
 
 read -p "What do you want the IP to be?: " newip
 echo "changing ip to '$newip'"
-echo -e "DEVICE=eth1\nIPADDR=$newip\nBROADCAST=192.168.56.255\nNETWORK=192.168.56.1\nNETMASK=255.255.255.0\nONBOOT=yes\nBOOTPROTO=none\nTYPE=Ethernet\nUSERCTL=no\nPEERDNS=yes\nIPV6INIT=no" > ~/tmp/ifcfg-eth1
-sudo mv ~/tmp/ifcfg-eth1 /etc/sysconfig/network-scripts/ifcfg-eth1
+echo -e "DEVICE=eth1\nIPADDR=$newip\nBROADCAST=192.168.56.255\nNETWORK=192.168.56.1\nNETMASK=255.255.255.0\nONBOOT=yes\nBOOTPROTO=none\nTYPE=Ethernet\nUSERCTL=no\nPEERDNS=yes\nIPV6INIT=no" > /tmp/ifcfg-eth1
+sudo mv /tmp/ifcfg-eth1 /etc/sysconfig/network-scripts/ifcfg-eth1

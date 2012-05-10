c3f469a5ab6146b71b82949fa88af270ee5b5ed1
Andy Wiens <andy@mindsandmachines.com>
2012-05-10 16:37:19 -0500

    made rvm install for all users so the ruby installations are available to root

--- mm-rvm-setup.sh
+++ mm-rvm-setup.sh
@@ -1,4 +1,5 @@
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+curl -L get.rvm.io | sudo bash -s stable
+usermod -a -G rvm andy
 sudo yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison
 source ~/.bashrc
 rvm install 1.9.3

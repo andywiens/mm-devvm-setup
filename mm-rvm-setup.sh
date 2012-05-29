fe311a124d00d67eb3bf3fa0df98dff7e270560d
Andy Wiens <awiens@gmail.com>
2012-05-29 16:59:08 -0500

    Update mm-rvm-setup.sh

--- mm-rvm-setup.sh
+++ mm-rvm-setup.sh
@@ -1,5 +1,5 @@
 curl -L get.rvm.io | sudo bash -s stable
-usermod -a -G rvm andy
+sudo usermod -a -G rvm andy
 sudo yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison
 source ~/.bashrc
 rvm install 1.9.3
